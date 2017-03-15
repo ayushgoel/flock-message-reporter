95141b589bb7bc5ad82ac65749267ffd7c5b1248
Ayush Goel <ayushgoel111@gmail.com>
2017-03-15 15:52:59 +0530

    Return string message on handling message action

--- src/start.py
+++ src/start.py
@@ -24,6 +24,7 @@ def eventsRoute():
             events.handle_app_uninstall(request)
         if name == "client.messageAction":
             events.handle_message_action(request)
+            return "Message reported 👍"
     return ""
 
 @bp.route("/UID", methods=['POST'])
